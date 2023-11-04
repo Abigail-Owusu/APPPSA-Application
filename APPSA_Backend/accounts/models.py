d6aac7f4e798a44c9c3737b534430eba41f7826d
Abigail-Owusu <abigail.owusu@ashesi.edu.gh>
2023-11-04 15:40:20 +0000

    registration complete

---
+++ APPSA_Backend/accounts/models.py
@@ -0,0 +1,92 @@
+
+# from django.contrib.auth.models import AbstractUser
+# from django.db import models
+
+
+# class CustomUser(AbstractUser):
+#     email = models.EmailField(unique=True)
+
+#     # Add custom fields here, if needed
+
+#     def __str__(self):
+#         return self.username
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+
+# Create your models here.
+
+
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, email, password, **extra_fields):
+#         if not email:
+#             raise ValueError("Email is required")
+#         # ensure that the email is case-insensitive
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+
+#     def create_superuser(self, email, password, **extra_fields):
+#         extra_fields.setdefault("is_staff", True)
+#         extra_fields.setdefault("is_superuser", True)
+
+#         if extra_fields.get("is_staff") is not True:
+#             raise ValueError("Superuser must have is_staff=True")
+
+#         if extra_fields.get("is_superuser") is not True:
+#             raise ValueError("Superuser must have is_superuser=True")
+
+#         return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser, PermissionsMixin):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    first_name = models.CharField(max_length=100, null=False, blank=False)
+    last_name = models.CharField(max_length=100, null=False, blank=False)
+    other_names = models.CharField(max_length=200, null=True, blank=True)
+    middle_name = models.CharField(max_length=100, null=True, blank=True)
+    maiden_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.EmailField(
+        max_length=200, null=False, blank=False, unique=True)
+    password = models.CharField(max_length=200, null=False, blank=False)
+    nationality = models.CharField(max_length=200, null=False, blank=False)
+    postal_code = models.CharField(max_length=20, null=False, blank=False)
+    zip_code = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=100, null=False, blank=False)
+    district = models.CharField(max_length=100, null=False, blank=False)
+    country = models.CharField(max_length=150, null=False, blank=False)
+    profession = models.CharField(max_length=200, null=False, blank=False)
+    current_organization = models.CharField(max_length=200, null=False, blank=False)
+    dob = models.DateField(null=False, blank=False)
+    tel_code = models.CharField(max_length=20, null=False, blank=False)
+    telephone = models.CharField( max_length=20, null=False, blank=False, unique=True)
+    additional_tel = models.CharField(
+        max_length=20, null=True, blank=True, unique=True)
+    first_name_next_of_kin = models.CharField(
+        max_length=200, null=False, blank=False)
+    last_name_next_of_kin = models.CharField(
+        max_length=200, null=False, blank=False)
+    tel_next_of_kin = models.CharField(max_length=20, null=False, blank=False)
+    year_group1 = models.IntegerField(null=False, blank=False)
+    year_group2 = models.IntegerField(null=True, blank=True)
+    chapter1 = models.CharField(max_length=200, null=False, blank=False)
+    chapter2 = models.CharField(max_length=200, null=True, blank=True)
+    chapter3 = models.CharField(max_length=200, null=True, blank=True)
+    house1 = models.CharField(max_length=200, null=False, blank=False)
+    house2 = models.CharField(max_length=200, null=True, blank=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    # objects = CustomUserManager()
+    # USERNAME_FIELD = 'email'
+    # REQUIRED_FIELDS = ['title', 'first_name', 'last_name', 'nationality', 'postal_code', 'city', 'district',
+    #                    'country', 'profession', 'current_organization', 'dob', 'tel_code', 'telephone',
+    #                    'email', 'first_name_next_of_kin', 'last_name_next_of_kin', 'tel_next_of_kin',
+    #                    'year_group1', 'chapter1', 'house1']
+
+    def __str__(self):
+        return self.email
---
+++ APPSA_Backend/accounts/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['title', 'first_name', 'last_name', 'nationality', 'postal_code', 'city', 'district',
+                  'country', 'profession', 'current_organization', 'dob', 'tel_code', 'telephone',
+                  'email', 'password', 'first_name_next_of_kin', 'last_name_next_of_kin', 'tel_next_of_kin',
+                  'year_group1', 'chapter1', 'house1']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser(
+            title=validated_data['title'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            nationality=validated_data['nationality'],
+            postal_code=validated_data['postal_code'],
+            city=validated_data['city'],
+            district=validated_data['district'],
+            country=validated_data['country'],
+            profession=validated_data['profession'],
+            current_organization=validated_data['current_organization'],
+            dob=validated_data['dob'],
+            tel_code=validated_data['tel_code'],
+            telephone=validated_data['telephone'],
+            email=validated_data['email'],
+            first_name_next_of_kin=validated_data['first_name_next_of_kin'],
+            last_name_next_of_kin=validated_data['last_name_next_of_kin'],
+            tel_next_of_kin=validated_data['tel_next_of_kin'],
+            year_group1=validated_data['year_group1'],
+            chapter1=validated_data['chapter1'],
+            house1=validated_data['house1']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+                        
