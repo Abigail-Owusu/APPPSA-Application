d58b5a86af37feff4dccfbc67d612f842a5a525c
Abigail-Owusu <abigail.owusu@ashesi.edu.gh>
2023-11-05 09:45:45 +0000

    add return message to Response

--- APPSA_Backend/accounts/views.py
+++ APPSA_Backend/accounts/views.py
@@ -19,7 +19,7 @@ def register_user(request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({'message': 'Sign up successful', 'data': serializer.data},  status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -38,7 +38,7 @@ def user_login(request):
         if user.check_password(password):
             print("User is authenticated")
             token, _ = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
+            return Response({'message': 'Login successful','token': token.key}, status=status.HTTP_200_OK)
 
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
