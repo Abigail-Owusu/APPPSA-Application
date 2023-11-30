40a1a7d9fd0389979402e7dbd44ad9997214c021
Abigail-Owusu <abigail.owusu@ashesi.edu.gh>
2023-11-30 21:32:18 +0000

    include name of person that made a comment

--- APPSA_Backend/discussion/views.py
+++ APPSA_Backend/discussion/views.py
@@ -11,10 +11,9 @@ def create_post(request):
     serializer = PostSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save(user=request.user)
-        authenticated_user = request.user.email
-        # Slice the email to get the username
-        auth_user_sliced = authenticated_user.find('@')
-        return Response({'message': f'{authenticated_user[:auth_user_sliced]} made a post successfully', 'data': serializer.data}, status=status.HTTP_201_CREATED)
+        first_name = request.user.first_name
+        last_name = request.user.last_name
+        return Response({'message': f'{first_name} {last_name} made a post successfully', 'data': serializer.data}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
@@ -22,10 +21,12 @@ def create_post(request):
 def comment_on_post(request):
     serializer = CommentSerializer(data=request.data)
     authenticated_user = request.user.email
+    first_name = request.user.first_name
+    last_name = request.user.last_name
     auth_user_sliced = authenticated_user.find('@')
     if serializer.is_valid():
         serializer.save(user=request.user)
-        return Response({'message': f'{authenticated_user[:auth_user_sliced]} commented successfully', 'data': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({'message': f'{first_name} {last_name} commented successfully', 'data': serializer.data}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
